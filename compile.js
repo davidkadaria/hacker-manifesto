27c3279813e231981bc85ccdbb15ca61a1aaff33
DavidKadaria <david.qada@gmail.com>
2023-12-25 21:18:52 +0400

    Add HTML file generator script

---
+++ compile.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const marked = require('marked');
+
+// Read the content of your README.md file
+const markdown = fs.readFileSync('README.md', 'utf8');
+
+// Use marked to convert Markdown to HTML
+const html = marked.parse(markdown);
+
+// Create the HTML structure with a head tag and add a link to the stylesheet
+const htmlWithStylesheet = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ჰაკერის მანიფესტი | The Hacker Manifesto</title>
+  <style>
+    body {
+      max-width: 990px;
+      padding: 15px !important;
+      margin-inline: auto !important;
+    }
+    body > p:last-child {
+      display: none !important;
+    }
+  </style>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.0/github-markdown-dark.css" integrity="sha512-QngYZiWDif1OjdGqaneXUjrCXGdv2bzecwqbFU2SvzlcHWuukimnIA2EtYHnZRow+3TzYgshlzCk+k9jPhXHMA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body class="markdown-body">
+  ${html}
+</body>
+</html>
+`;
+
+// Write the updated HTML content to a new file (e.g., index.html)
+fs.writeFileSync('index.html', htmlWithStylesheet);
